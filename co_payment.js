dca406538dee8cb0204af163e835fdd8ab791aef
austin inmon <Ainmon987@west-mec.org>
2019-04-18 14:08:54 -0700

    Final commit
    retrieve whether the sum of the digits is divisible by 10. retrieve the even number digits. retrieve the odd number digits. Ready for review.

--- co_payment.js
+++ co_payment.js
@@ -122,6 +122,8 @@ function validateNumber() {
             cardNumber.setCustomValidity("Enter your card number");
       } else if (cardNumber.validity.patternMismatch) {
             cardNumber.setCustomValidity("Enter a valid card number");
+      } else if (luhn(cardNumber.value) === false) {
+            cardNumber.setCustomValidity("Enter legitimate card number");
       } else {
             cardNumber.setCustomValidity("");
       }
@@ -157,9 +159,17 @@ function sumDigits(numStr) {
 }
 
 function luhn(idNum) {
+      var string1 = "";
+      var string2 = "";
       //retreive the odd number digits
+      for (var i = idNum.length - 1; i >= 0; i -= 2) {
+            string1 += idNum.charAt(i);
+      }
 
       //retreive the even number digits
-
+      for (var i = idNum.length - 2; i >= 0; i -= 2) {
+            string2 += 2 * idNum.charAt(i);
+      }
       //retreive whether the sum of the digits is divisable by 10
+      return sumDigits(string1 + string2) % 10 === 0;
 }
